50ee31a2e45b5c8e4a955c919b5157d13e9ca034
Arelav72 <163861117+Arelav72@users.noreply.github.com>
2024-03-22 14:33:05 +0300

    add all cycle code

--- main.py
+++ main.py
@@ -23,7 +23,21 @@ color = (random.randint(0,255), random.randint(0,255), random.randint(0,255)) # 
 
 running = True
 while running:
-    pass
+  screen.fill(color)  # pандомная заливка окна
+  screen.blit(target, (target_x, target_y))  # мишение
+  pygame.display.update() # Обновление окна
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+    if event.type == pygame.MOUSEBUTTONDOWN:
+      mouse_x, mouse_y = pygame.mouse.get_pos()
+      if target_x <= mouse_x <= target_x + target_width and target_y <= mouse_y <= target_y + target_height:
+        # pygame.mixer.Sound.play(pygame.mixer.Sound('sound/click.wav'))
+        target_x = random.randint(0, SCREEN_WIDTH - target_width) #  координаты мишени по Х
+        target_y = random.randint(0, SCREEN_HEIGHT - target_height) #  координаты мишени по У
+  screen.blit(target, (target_x, target_y)) # отображаем мишень на экране игры
+
+  pygame.display.update() # Обновление окна
 
 
 pygame.quit()
